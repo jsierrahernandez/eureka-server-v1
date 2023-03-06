0cf822e77be292e367dcd0d2d7633db08e668b44
Jonathan Sierra Hernández <j.sierra@indizen.com.es>
2023-03-06 13:02:48 +0100

    first commit

---
+++ src/test/java/com/indizen/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indizen.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
